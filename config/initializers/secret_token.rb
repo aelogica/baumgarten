d775f979319dcde6e568662935cc28953f2c7843
Travis Vocino <travis@vocino.com>
2010-09-28 17:05:32 -0400

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baumgarten::Application.config.secret_token = 'bc7cfc064d224f84d40714694d3c33ee6978b7c490f05a4aa7b23d273eb7eb77ac18e7159902fbea2cffcd4784c42c04ad031897a46af1df1c4433d82d9891ea'
